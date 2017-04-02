5fa50db5a928c63602bd2b5fb6ce47f3549c58c5
Milivoj Cosich <lcdprueil@macbook-air-de-lcdprueil.home>
2017-04-02 23:30:36 +0200

    Mise en forme du Chat

--- public/client.js
+++ public/client.js
@@ -1,81 +1,73 @@
- // Connexion    socket.io
-            var socket = io.connect('http://milivoy.screeb.io');
+// Connexion    socket.io
+var socket = io.connect('http://milivoy.screeb.io');
 
-            // On demande le pseudo, on l'envoie au serveur et on l'affiche dans le titre
-            var pseudo = prompt('Quel est votre pseudo ?');
-            socket.emit('nouveau_client', pseudo);
-            document.title = pseudo + ' - ' + document.title;
+// On demande le pseudo, on l'envoie au serveur et on l'affiche dans le titre
+var pseudo = prompt('Quel est votre pseudo ?');
+socket.emit('nouveau_client', pseudo);
+document.title = pseudo + ' - ' + document.title;
+document.getElementById("user").innerHTML = pseudo;
 
-            // Quand on re  oit un message, on l'ins  re dans la page
-            socket.on('message', function(data) {
+// Quand on re  oit un message, on l'ins  re dans la page
+socket.on('message', function (data) {
 
-                try{
+    try {
+        var nowtimeA = data.date.split("T");
+        var nowtimeB = nowtimeA[1].split(":");
+        var heure = Number(nowtimeB[0]) + 2;
+        var nowtime = nowtimeA[0] + " || " + heure + ":" + nowtimeB[1];
+        insereMessage(data.pseudo, data.message, nowtime);
+    }
+    
+    catch(err) {
+        console.log(err + " : " + nowtimeB);
+    }
+})
 
-                        var nowtimeA = data.date.split("T");
-                        var nowtimeB = nowtimeA[1].split(":");
-                        var heure = Number(nowtimeB[0])+2;
-                        var nowtime = nowtimeA[0] + " || " + heure + ":" + nowtimeB[1];
-                        insereMessage(data.pseudo, data.message, nowtime);
-                }
-                catch(err){
-                console.log(err + " : " + nowtimeB);
-                }
+// Quand un nouveau client se connecte, on affiche l'information
+socket.on('nouveau_client', function(pseudo) {
+    $('#zone_chat').prepend('<p><em>' + pseudo + ' a rejoint le Chat !</em></p>');
+})
 
-        })
-
-            // Quand un nouveau client se connecte, on affiche l'information
-            socket.on('nouveau_client', function(pseudo) {
-                $('#zone_chat').prepend('<p><em>' + pseudo + ' a rejoint le Chat !</em></p>');
-            })
-
-            // Lorsqu'on envoie le formulaire, on transmet le message et on l'affiche sur la page
-            $('#formulaire_chat').submit(function () {
-                var message = $('#message').val();
-                var nowtimeB = new Date();
-                var jour = nowtimeB.getDate();
-                var mois = nowtimeB.getMonth()+1;
-                var annee = 2000+nowtimeB.getYear()-100;
-                var heure = nowtimeB.getHours();
-                var minutet = nowtimeB.getMinutes();
-
-
-             var nowtime = annee + "-" + format00(mois) + "-" + format00(jour) + " || " + format00(heure) + ":" + format00(minutet);
+// Lorsqu'on envoie le formulaire, on transmet le message et on l'affiche sur la page
+$('#formulaire_chat').submit(function () {
+    var message = $('#message').val();
+    var nowtimeB = new Date();
+    var jour = nowtimeB.getDate();
+    var mois = nowtimeB.getMonth()+1;
+    var annee = 2000+nowtimeB.getYear()-100;
+    var heure = nowtimeB.getHours();
+    var minutet = nowtimeB.getMinutes();
+    var nowtime = annee + "-" + format00(mois) + "-" + format00(jour) + " || " + format00(heure) + ":" + format00(minutet);
 
 
-                socket.emit('message', message, nowtimeB); // Transmet le message aux autres
+    socket.emit('message', message, nowtimeB); // Transmet le message aux autres
 
-                insereMessage(pseudo, message, nowtime); // Affiche le message aussi sur notre page
-                $('#message').val('').focus(); // Vide la zone de Chat et remet le focus dessus
-                return false; // Permet de bloquer l'envoi "classique" du formulaire
-            });
-
-
-            // Ajoute un message dans la page
-            function insereMessage(pseudo, message, date) {
-                $('#zone_chat').prepend('<p><h3>' + date + '</h3> <strong>' + pseudo + '</strong> ' + message + '</p>');
-            }
-            // Fonction trasformation 00
-            
-
-
-
-
+    insereMessage(pseudo, message, nowtime); // Affiche le message aussi sur notre page
+    $('#message').val('').focus(); // Vide la zone de Chat et remet le focus dessus
+    return false; // Permet de bloquer l'envoi "classique" du formulaire
+});
+   
+// Ajoute un message dans la page
+function insereMessage(pseudo, message, date) {
+    $('#zone_chat').prepend('<div class="msg"><div class="msgvrai"><div class="entete"><div class="msguser">' + pseudo + '</div><div class="msgdate">' + date +  '</div></div>' + message + '</div></div>');
+}
+// Fonction trasformation 00
 function format00(x){
 	switch(x) {
-                        case 0:
-                        case 1:
-                        case 2:
-                        case 3:
-                        case 4:
-                        case 5:
-                        case 6:
-                        case 7:
-                        case 8:
-                        case 9:
-                                return '0'+x
-                        break;
+        case 0:
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+        case 7:
+        case 8:
+        case 9:
+            return '0'+x
+        break;
 
-                        default:
-                                return x
-                }
+        default:
+            return x
+    }
 };
