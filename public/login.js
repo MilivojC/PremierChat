c630b0cb0c711d71a1b34bf28602701b15c8f09e
Milivoj Cosich <lcdprueil@macbook-air-de-lcdprueil.home>
2017-04-06 14:52:30 +0200

    On en a marre

--- public/login.js
+++ public/login.js
@@ -5,5 +5,5 @@ $('#formulaire_login').submit(function () {
 
     $('#Username').val('').focus(); 
     $('#password').val(''); // Vide les zones de renseignement et remet le focus sur identifiant
-
+    return;
 });
