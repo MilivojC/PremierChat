3393401e8f939264f1b94c46d57a139ac4485031
Milivoj Cosich <lcdprueil@macbook-air-de-lcdprueil.home>
2017-04-06 23:22:18 +0200

    Success maj

--- server.js
+++ server.js
@@ -134,7 +134,7 @@ app.get('/home', function (req, res) {
 
 app.post('/home', upload.array(), function (req, res) { 
     
-    req.session._expires:0;
+    req.session._expires=0;
     console.log(req.session);
     
 });   
