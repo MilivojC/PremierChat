c311cde8967659011c58ea756d4aaa45c1fca2fa
Milivoj Cosich <lcdprueil@airdelcdprueil.home>
2017-04-06 04:49:50 +0200

    Enfin la victoire...pour bientot...Pas encore...encore pas du tout...et la

--- server.js
+++ server.js
@@ -67,7 +67,66 @@ app.get('/home', function (req, res) {
         
         res.sendFile(__dirname + '/public/home.html');
         app.io.sockets.on('connection', function (socket, pseudo) {
-            socket.emit('acceptationChat', req.session.user);});
+
+        // ECOUTE CONCERNANT LE CHAT
+
+// ---- CONNEXION AU CHAT DUN NOUVEAU CLIENT
+    socket.on('ouvertureChat', function() {
+ 
+
+        var nomUtilisateur = req.session.user;
+//        pseudo = ent.encode(pseudo);
+       socket.pseudo = nomUtilisateur;
+        socket.emit('acceptationChat', nomUtilisateur)
+        socket.broadcast.emit('nouveau_client', nomUtilisateur);
+	   //Code pour la recuperation des messages dans la base de donnée
+        var pg = require('pg');
+        var conString = "postgres://postgres@localhost:5432/db_work";
+	   var client2 = new pg.Client(conString);
+	   client2.connect();
+	   var query = client2.query("SELECT * FROM messages");
+	
+	query.on('row', function(row) {
+		try{
+			socket.emit('message', {pseudo: row.utilisateur, message: row.message, date: row.date});
+		}
+		catch(err){
+			console.log("Problème sur le broadcast");
+		}
+
+
+	});
+	
+	query.on('end', function() {
+	    client2.end();
+	});
+          
+    });
+
+// ---- NOUVEAU MESSAGE    
+    socket.on('message', function (message, date) {
+        // Dès qu'on reçoit un message, on récupère le pseudo de son auteur et on le transmet aux autres personnes
+        message = ent.encode(message);
+        socket.broadcast.emit('message', {pseudo: socket.pseudo, message: message, date: date});
+        //Code pour l'enregistrement des messages dans la base de donnée
+	   var pg = require('pg');
+	   var conString = "postgres://postgres@localhost:5432/db_work";
+        var client = new pg.Client(conString);
+	   client.connect();
+	   client.query("INSERT INTO messages(utilisateur, message, date) VALUES($1, $2, $3)",[ socket.pseudo, message, date]);
+    }); 
+
+    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        });
         
     }
     else {
