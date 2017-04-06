ea15d9211074dc47449d13270d5ae7bda9332dbe
Milivoj Cosich <lcdprueil@macbook-air-de-lcdprueil.home>
2017-04-07 01:28:38 +0200

    Success maj

--- public/login.js
+++ public/login.js
@@ -2,15 +2,17 @@ var socket = io.connect('http://milivoy.screeb.io');
 
 //socket.emit('jesuisla');
 $('#formulaire_login').submit(function () {
-    $('#Username').val('').focus(); 
-    $('#password').val(''); // Vide les zones de renseignement et remet le focus sur identifiant
-            socket.emit('verification');
-    return;
-    
-    
+    return submAt();   
 });
 
 
 socket.on('refus', function(){
   $('#console').prepend('<p style="color:red;">Identifiant ou mot de passe incorrect!</p>');  
 });
+
+
+function submAt(){   
+    $('#Username').val('').focus(); 
+    $('#password').val(''); // Vide les zones de renseignement et remet le focus sur identifiant
+            socket.emit('verification');
+};
