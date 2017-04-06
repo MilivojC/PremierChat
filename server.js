f1c73252b1a03cad0b9cd3b89c7526119b57ae82
Milivoj Cosich <lcdprueil@macbook-air-de-lcdprueil.home>
2017-04-07 00:32:50 +0200

    Success maj

--- server.js
+++ server.js
@@ -91,11 +91,11 @@ app.post('/login', upload.array(), function(req, res) {
         
    //     else --> Sinon on affiche une erreur d'authentification avec le websocket
         sess = req.session; //Pour cela on affecte la session a la variable sess qui sera utilisé par le websocket.    
-        res.end();
+        
 
 	});  
 
-
+res.end();
 });
 
 //Renvoie toutes les demandes '/' sur '/home' -> permet de shinté les problèmes avec index.html
