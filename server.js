742fa27e2c679ed5133a2845e7ef460880a5f613
Milivoj Cosich <lcdprueil@airdelcdprueil.home>
2017-04-06 04:38:52 +0200

    Enfin la victoire...pour bientot...Pas encore...encore pas du tout...et la

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.all('/',function(req,res){res.redirect('/home');});
 app.use(express.static(__dirname + '/public'));
-app.io.sockets.on('connection', function (socket, pseudo) {};
+app.io.sockets.on('connection', function (socket, pseudo) {});
 
 
 
