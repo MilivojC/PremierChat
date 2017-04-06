ecaed16d83cde42428f26ba8d4125876f2214605
Milivoj Cosich <lcdprueil@macbook-air-de-lcdprueil.home>
2017-04-06 18:04:05 +0200

    Cest bon la

--- AuthMili.js
+++ AuthMili.js
@@ -1,12 +1,14 @@
 var verif = function(nom, pass) {
-  var ver = function(){
+  
+    
+
     var pg = require('pg'),
         conString = "postgres://postgres@localhost:5432/db_work",
         client = new pg.Client(conString);
         client.connect();
     var query = client.query("SELECT * FROM identification WHERE nigol ='" + nom +"'");
 
-    query.on('row', function(row, res) {
+    var ver = query.on('row', function(row, res) {
 		
         console.log(row.drowssap == pass);
         console.log(row.nigol == nom);
@@ -16,14 +18,21 @@ var verif = function(nom, pass) {
             return true;
         
         }
+        
+        else {
+            
+            retun false;
+        }
 
 	});
 	
 	query.on('end', function() {
 	    client.end();
 	});
+  
+    
+    return [ver, nom];
 };
-    console.log([ver(), nom]);
-    return [ver(), nom];
-};
+   
+
 exports.verif = verif;
