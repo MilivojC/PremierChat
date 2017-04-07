8821e7ac88d8dd68c6128398a017d74b479a7683
Milivoj Cosich <lcdprueil@macbook-air-de-lcdprueil.home>
2017-04-07 02:42:36 +0200

    Jquery mon amour

--- public/login.js
+++ public/login.js
@@ -3,18 +3,12 @@ var socket = io.connect('http://milivoy.screeb.io');
 //socket.emit('jesuisla');
 $('#formulaire_login').submit(function () {
 
-    
+    var xhr = getXMLHttpRequest();
+    xhr.open("POST", "/login", true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.send("Username=" + $('#Username')+ "&password=" + $('#password'));
 
-    $.post("http://milivoy.screeb.io/login",
-    {
-        Username: $('#Username'),
-        password: $('#password')
-    });
 
-    
-    
-    
-    
  //   socket.emit('verification');  
 //    $('#Username').val('').focus(); 
 //    $('#password').val(''); // Vide les zones de renseignement et remet le focus sur identifiant
@@ -25,3 +19,26 @@ socket.on('refus', function(){
   $('#console').prepend('<p style="color:red;">Identifiant ou mot de passe incorrect!</p>');  
 });
 
+
+
+
+function getXMLHttpRequest() {
+	var xhr = null;
+	
+	if (window.XMLHttpRequest || window.ActiveXObject) {
+		if (window.ActiveXObject) {
+			try {
+				xhr = new ActiveXObject("Msxml2.XMLHTTP");
+			} catch(e) {
+				xhr = new ActiveXObject("Microsoft.XMLHTTP");
+			}
+		} else {
+			xhr = new XMLHttpRequest(); 
+		}
+	} else {
+		alert("Votre navigateur ne supporte pas l'objet XMLHTTPRequest...");
+		return null;
+	}
+	
+	return xhr;
+}
