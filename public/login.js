ade8c7c9a3637c1f01add28d6aae7cc89a666fb6
Milivoj Cosich <lcdprueil@macbook-air-de-lcdprueil.home>
2017-04-07 03:22:47 +0200

    Jquery mon amour

--- public/login.js
+++ public/login.js
@@ -2,16 +2,10 @@ var socket = io.connect('http://milivoy.screeb.io');
 
 //socket.emit('jesuisla');
 $('#formulaire_login').submit(function () {
-    return;
-});
-
-$('#formulaire_login').submit(function () {
-
-
-    $('#Username').val('').focus(); 
-    $('#password').val(''); // Vide les zones de renseignement et remet le focus sur identifiant
+ //   $('#Username').val('').focus(); 
+//    $('#password').val(''); // Vide les zones de renseignement et remet le focus sur identifiant
   socket.emit('verification');
-    return false;
+    return;
 });
 
 
