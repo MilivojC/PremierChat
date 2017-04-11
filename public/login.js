28bd07275385328f32e9614d4ebe181a9af47553
Milivoj Cosich <lcdprueil@macbook-air-de-lcdprueil.home>
2017-04-12 01:22:28 +0200

    Integration lien vendHq3

--- public/login.js
+++ public/login.js
@@ -1,5 +1,5 @@
 var socket = io.connect('http://milivoy.screeb.io');
-/*
+
 function soumission() {
     document.getElementById("#formulaire_login").submit();  
     //socket.emit('verification'); 
@@ -8,7 +8,12 @@ function soumission() {
     //document.getElementById("#password").reset(); // Vide les zones de renseignement et remet le focus sur identifiant
     
 };
-*/
+
+function aftersoum(){
+    document.getElementById("#Username").value="";
+    document.getElementById("#password").value="";
+    socket.emit('verification');
+}
 
 
 
@@ -24,6 +29,8 @@ $('#formulaire_login').submit(function () {
 return false;
 });
 */
+/*
+
 $('#formulaire_login').submit(function () {
     document.getElementById("#formulaire_login").submit();
     $('#Username').val('').focus(); 
@@ -33,7 +40,7 @@ $('#formulaire_login').submit(function () {
  return false;
 });
 
-
+*/
 socket.on('refus', function(){
   $('#console').prepend('<p style="color:red;">Identifiant ou mot de passe incorrect!</p>');  
 });
