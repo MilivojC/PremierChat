abd1051ca178989f662c6b1dabfafeb407271cb1
Milivoj Cosich <lcdprueil@macbook-air-de-lcdprueil.home>
2017-04-11 16:52:46 +0200

    Integration lien vendHq3

--- public/login.js
+++ public/login.js
@@ -13,7 +13,9 @@ $('#formulaire_login').submit(function () {
          }
     console.log("La fonction 1 est jouee");
 return false;
-},function () {
+});
+
+$('#formulaire_login').submit(function () {
  
     $('#Username').val('').focus(); 
     $('#password').val(''); // Vide les zones de renseignement et remet le focus sur identifiant
