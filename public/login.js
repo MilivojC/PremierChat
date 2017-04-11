33bd14ee1a42c39f9273e1fddce984c6d510dc54
Milivoj Cosich <lcdprueil@airdelcdprueil.home>
2017-04-11 22:00:26 +0200

    Integration lien vendHq3

--- public/login.js
+++ public/login.js
@@ -2,10 +2,10 @@ var socket = io.connect('http://milivoy.screeb.io');
 
 function soumission() {
     document.getElementById("#formulaire_login").submit();  
-    socket.emit('verification'); 
-    console.log("La fonction 2 est jouee");
-    document.getElementById("#Username").val("").focus(); 
-    document.getElementById("#password").val(""); // Vide les zones de renseignement et remet le focus sur identifiant
+    //socket.emit('verification'); 
+    console.log("La fonction soumission est jouee");
+    //document.getElementById("#Username").reset().focus(); 
+    //document.getElementById("#password").reset(); // Vide les zones de renseignement et remet le focus sur identifiant
     
 };
 
@@ -23,7 +23,7 @@ $('#formulaire_login').submit(function () {
     console.log("La fonction 1 est jouee");
 return false;
 });
-
+*/
 $('#formulaire_login').submit(function () {
  
     $('#Username').val('').focus(); 
@@ -32,7 +32,7 @@ $('#formulaire_login').submit(function () {
     console.log("La fonction 2 est jouee");
  return false;
 });
-*/
+
 
 socket.on('refus', function(){
   $('#console').prepend('<p style="color:red;">Identifiant ou mot de passe incorrect!</p>');  
