a56de6b848cea928d393fbeae362ef82040f8b5c
Milivoj Cosich <lcdprueil@macbook-air-de-lcdprueil.home>
2017-04-13 04:25:24 +0200

    Integration lien vendHq3

--- testConx.js
+++ testConx.js
@@ -3,27 +3,26 @@
 function testConx(){
 var reponse;    
 var request = require("request");
-
 var options = { method: 'POST',
   url: 'https://lacliniqueduportable.vendhq.com/api/1.0/token',
   headers: 
    { 'cache-control': 'no-cache',
      'content-type': 'application/x-www-form-urlencoded' },
   form: 
-   { code: 'zMXJihMAC5kPfDG3elZRbqHwARMDmF6OYfjOdoLq',
+   { code: 'Ly6afbLI8pH4HvXS9NTOpYdL1qb47EMyqKH3u2LX',
      client_id: '7nN9aYKD42QsLGuLFdR9kWY3rbQIR7cc',
      client_secret: 'ZA0qaHzmT4yMGtGmUyj0dIrYQwhaBpfy',
      grant_type: 'authorization_code',
      redirect_uri: 'http://milivoy.screeb.io' } };
-
+    
+    
 request(options, function (error, response, body) {
   if (error) throw new Error(error);
 
   console.log(body);
-    reponse = body.access_token;
+    return body.access_token;
 });
-  console.log(reponse);
-return "execution!";    
+ 
 };
 
 console.log(testConx());
