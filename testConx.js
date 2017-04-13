f8419bd2dc9a5219a84e895c960f710463d99cf8
Milivoj Cosich <lcdprueil@macbook-air-de-lcdprueil.home>
2017-04-13 04:13:48 +0200

    Integration lien vendHq3

--- testConx.js
+++ testConx.js
@@ -2,42 +2,27 @@
 
 function testConx(){
 var reponse;    
-var code = 'kcOOYxhbTwOzKwjPTQcDgRMG1XNbQOFRkt8kEtsg';
-var codeV =  code,
-    client_idV = '7nN9aYKD42QsLGuLFdR9kWY3rbQIR7cc',
-    client_secretV = 'ZA0qaHzmT4yMGtGmUyj0dIrYQwhaBpfy',
-    redirect_uriV = 'http://milivoy.screeb.io';
-        //var request = require("request");
-    var rp = require('request-promise');
-    var options = { 
-                method: 'POST',
-                url: 'https://lacliniqueduportable.vendhq.com/api/1.0/token',
-                headers: 
-                    { 
-                        'cache-control': 'no-cache',
-                        'content-type': 'application/x-www-form-urlencoded' 
-                    },
-                form: 
-                    { 
-                        code: codeV,
-                        client_id: client_idV,
-                        client_secret: client_secretV,
-                        grant_type: 'authorization_code',
-                        redirect_uri: redirect_uriV 
-                    },
-                json: true
-        };
-    
-rp(options).then(function(body){
-        reponse = body;
-        console.body(body);
-    }).catch(function (err){
-        console.log("Erreur rp");
-    });
-    
-    console.log(options);
-    return reponse;
-    
-    };
+var request = require("request");
+
+var options = { method: 'POST',
+  url: 'https://lacliniqueduportable.vendhq.com/api/1.0/token',
+  headers: 
+   { 'cache-control': 'no-cache',
+     'content-type': 'application/x-www-form-urlencoded' },
+  form: 
+   { code: 'bINd4ofGI2eSaoUbb4muswiWaeJUIlnou8FpPzJh',
+     client_id: '7nN9aYKD42QsLGuLFdR9kWY3rbQIR7cc',
+     client_secret: 'ZA0qaHzmT4yMGtGmUyj0dIrYQwhaBpfy',
+     grant_type: 'authorization_code',
+     redirect_uri: 'http://milivoy.screeb.io' } };
+
+request(options, function (error, response, body) {
+  if (error) throw new Error(error);
+
+  console.log(body);
+});
+  
+return "execution!";    
+};
 
 console.log(testConx());
