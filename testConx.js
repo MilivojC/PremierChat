23dd0723049c035a47c0231d4dad010388a5e72d
Milivoj Cosich <lcdprueil@macbook-air-de-lcdprueil.home>
2017-04-13 15:36:34 +0200

    Integration lien vendHq3

--- testConx.js
+++ testConx.js
@@ -1,13 +1,6 @@
-var bodyreq;
-function general(data, callback){
-    
-   callback(data); 
-    return data;
-}
 
-
-function testConx(data){
-    
+function testConx(){
+var data = 'Pas de token';    
 var request = require("request");
 var options = { method: 'POST',
   url: 'https://lacliniqueduportable.vendhq.com/api/1.0/token',
@@ -15,7 +8,7 @@ var options = { method: 'POST',
    { 'cache-control': 'no-cache',
      'content-type': 'application/x-www-form-urlencoded' },
   form: 
-   { code: 'iQcOnobz5TmEz21Xh1PZovnUsOJsEaNUxdgZ2TTc',
+   { code: 'ebmC8qLP6xs1pCfbHatslrL0UN8RQLU7HytZJS6R',
      client_id: '7nN9aYKD42QsLGuLFdR9kWY3rbQIR7cc',
      client_secret: 'ZA0qaHzmT4yMGtGmUyj0dIrYQwhaBpfy',
      grant_type: 'authorization_code',
@@ -24,11 +17,25 @@ var options = { method: 'POST',
     
 request(options, function (error, response, body) {
   if (error) throw new Error(error);
-
-
-    data = body;
+data = body;
 });
- 
+  
+var i =0;    
+while (i == 0){
+    if (data=='Pas de token'){
+        console.log("data non affectée");
+        
+    }
+    else {
+        console.log ("data affecte!");
+        i=1;
+        console.log(data);
+        return data;
+    }
+}    
+    
+    
+
 };
 
 
