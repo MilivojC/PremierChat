fbe5f5d783c05ca27c06dedbe61ea6115b5dd0f1
Milivoj Cosich <lcdprueil@macbook-air-de-lcdprueil.home>
2017-04-14 13:52:25 +0200

    On avance dans la creation des tickets

--- server.js
+++ server.js
@@ -276,7 +276,7 @@ io.sockets.on('connection', function (socket, pseudo) {
         
         // ON VA DANS UN PREMIER TEMPS LANCE LA REQUETE POUR AVOIR LE NOMBRE DE PAGE
         var noPage = 1;
-
+        var nbrPages = 1;
         do {
         var options = { method: 'GET',
             url: 'https://' + keys.prefix_client + '.vendhq.com/api/register_sales?page=' + noPage,
@@ -290,7 +290,7 @@ io.sockets.on('connection', function (socket, pseudo) {
         request(options, function (error, response, body) {
             
             if (error) throw new Error(error);
-
+            nbrPages = JSON.parse(body).pagination.page;
             
             var estVide = JSON.parse(body).register_sales.length;
             console.log("Je suis a la fin de la requete et je dis que le no de page est " + JSON.parse(body).pagination.page );
@@ -307,7 +307,7 @@ io.sockets.on('connection', function (socket, pseudo) {
             });
             
             noPage++;
-            } while (noPage <= 4);
+            } while (noPage <= nbrPages);
         
         
         
